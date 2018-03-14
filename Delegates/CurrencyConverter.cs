49a4c4fd9f8f133430ced0afd080d03dad9d689d
PGS-SOFT\pbielecki <pbielecki@pgs-soft.com>
2018-03-14 11:41:40 +0100

    Delegate example

---
+++ Delegates/CurrencyConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Delegates
+{
+    public class CurrencyConverter
+    {
+        public double ZlotyToDolar (int amount)
+        {
+            const double exchangeRate = 0.29;
+            var result = amount * exchangeRate;
+
+            return result;
+        }
+
+        public double DolarToZloty(int amount)
+        {
+            const double exchangeRate = 3.41;
+            var result = amount * exchangeRate;
+
+            return result;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        private delegate double CurrencyDelegate(int amount);
+
+        private static void Main()
+        {
+            var currencyConverter = new CurrencyConverter();
+            var currencyDelegate = new CurrencyDelegate(currencyConverter.DolarToZloty);
+            var dolarExchangeRate = currencyDelegate(1);
+
+            Console.WriteLine($"Obecny kurs dolara to: {dolarExchangeRate}");
+            Console.WriteLine($"100 zł to: {currencyConverter.ZlotyToDolar(100)} USD");
+            Console.ReadKey();
+        }
+    }
+}
